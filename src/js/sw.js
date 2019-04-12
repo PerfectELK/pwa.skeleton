20e996458bfaff873d2672b48dcf1f441d8f738c
perfect-elk <perfect-elk@perfect-elk.ru>
2019-04-12 13:50:25 +0300

    two

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,5 +1,11 @@
+'use strict';
+
 importScripts('/pwa.skeleton/sw-toolbox.js');
 
-toolbox.precache(["/index.html","/pwa.skeleton/style/style.css"]);
+toolbox.precache(["/pwa.skeleton/index.html","/pwa.skeleton/style/style.css"]);
+
 toolbox.router.get('/pwa.skeleton/src/images/*', toolbox.cacheFirst);
-toolbox.router.get('/pwa.skeleton/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
+
+toolbox.router.get('/pwa.skeleton/*', toolbox.networkFirst, {
+    networkTimeoutSeconds: 5
+});
