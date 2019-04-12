6dd2bb42589fc33d637ec1dbfd89ee8906688dd2
perfect-elk <perfect-elk@perfect-elk.ru>
2019-04-12 13:42:54 +0300

    two

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,5 +1,5 @@
 importScripts('/pwa.skeleton/sw-toolbox.js');
 
-toolbox.precache(["/index.html","/style/style.css"]);
-toolbox.router.get('/src/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
+toolbox.precache(["/index.html","/pwa.skeleton/style/style.css"]);
+toolbox.router.get('/pwa.skeleton/src/images/*', toolbox.cacheFirst);
+toolbox.router.get('/pwa.skeleton/*', toolbox.networkFirst, {networkTimeoutSeconds: 5});
